19742e9c217da4a2a25cd4b211a1e482bb3ba815
ayush sharma <ayush.aceit@gmail.com>
2015-12-04 17:24:11 +0530

    Rangoli done

--- rangoli.py
+++ rangoli.py
@@ -48,15 +48,41 @@ The center of the rangoli has the first alphabet letter a, and the boundary has 
 """
 
 import string
+from collections import defaultdict
+d = defaultdict(list)
 num = input()
 alph = list(string.ascii_lowercase)
 print alph
 
 
-for i in xrange(num):
-	print ('#'*i).rjust(num*2,'-')+'#'+('#'*i).ljust(num*2,'-')
+# for i in xrange(num):
+# 	print ((alph[num])+'-'*i).rjust(num*2,'-')+'#-'+('#-'*i).ljust(num*2,'-')
 
-for i in xrange(1,num):
-	print (('#'*(num-i-1)).rjust(num*2,'-')+'#'+('#'*(num-i-1)).ljust(num*2,'-'))
+# for i in xrange(1,num):
+# 	print (('#-'*(num-i-1)).rjust(num*2,'-')+'#-'+('#-'*(num-i-1)).ljust(num*2,'-'))
 
+for i in range(1,(2*num-1)+1):
+	for j in range(1,(2*(2*num-1)-1)+1):
+		d[i].append('-')
 
+k = (len(d)*2)//2
+temp = (len(d)//2 )
+for i in range(1,len(d)+1):
+	if i <= (len(d)+1)//2:
+		index = num
+		middle  = (len(d[i])+1)//2
+		for j in range(k,k+4*i-3,2):
+			d[i][j-1] = alph[index-1]
+			if j >= middle:
+				index = index +1
+			else:
+				index = index -1
+		k = k-2
+	else:
+		d[i] = d[temp]
+		temp = temp -1
+		
+print d
+
+for i in range(1,len(d)+1):
+	print ''.join(d[i])
