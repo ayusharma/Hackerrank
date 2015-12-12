2ec7a885ab1bca35ef2d2918a34d623336ece64d
ayush sharma <ayush.aceit@gmail.com>
2015-12-12 09:38:00 +0530

    Hex Char must be capital

--- string_formatting.py
+++ string_formatting.py
@@ -7,8 +7,7 @@ bnum = len(str("{0:b}".format(num)))
 
 
 for i in xrange(1,num+1):
-	print ("{0:d}".format(i)).rjust(bnum)+'\t'+("{0:o}".format(i)).rjust(bnum)+'\t'+("{0:x}".format(i)).rjust(bnum)+'\t'+("{0:b}".format(i)).rjust(bnum)
-	# print ("{0:d}".format(i)).rjust(bnum)+' '+("{0:o}".format(i)).rjust(bnum)+' '+("{0:x}".format(i)).rjust(bnum)+' '+("{0:b}".format(i)).rjust(bnum)
+	print ("{0:d}".format(i)).rjust(bnum)+'\t'+("{0:o}".format(i)).rjust(bnum)+'\t'+("{0:X}".format(i)).rjust(bnum)+'\t'+("{0:b}".format(i)).rjust(bnum)
 
 
 
