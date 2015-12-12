97d506eef4ec9bc2c93c9cf83f5306cdcca3d505
ayush sharma <ayush.aceit@gmail.com>
2015-12-12 13:57:23 +0530

    set_subset

--- set_subset.py
+++ set_subset.py
@@ -53,5 +53,6 @@ Hence, set A is subset of set B
 """
 
 for i in range(int(raw_input())): #More than 4 lines will result in 0 score. Blank lines won't be counted.  
-    a = int(raw_input()); A = set(raw_input().split())
-    b = int(raw_input()); B = set(raw_input().split())
+    a = int(raw_input()); A = set(raw_input().split());b = int(raw_input()); B = set(raw_input().split())
+    if A.issubset(B) : print 'True'
+    else : print 'False'
