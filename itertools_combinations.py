6b5fb65c5fa819df0f99122af36a090ab9ddd06a
ayush sharma <ayush.aceit@gmail.com>
2015-12-17 08:37:36 +0530

    used groupby method

--- itertools_combinations.py
+++ itertools_combinations.py
@@ -1,10 +1,9 @@
 from itertools import combinations
 I = raw_input().split()
 m = list()
-for i in xrange(1,int(I[1])+1):
+for i in xrange(1,int(I[1])):
     m.append(list(map(''.join,combinations(I[0],i))))
 
 for i in m:
-	i.sort()
 	for k in i:
 		print k
