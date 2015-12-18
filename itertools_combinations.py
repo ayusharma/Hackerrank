00544a15ad2081d41006597f276e219c85530212
ayush sharma <ayush.aceit@gmail.com>
2015-12-18 09:20:24 +0530

    take sorted input

--- itertools_combinations.py
+++ itertools_combinations.py
@@ -1,7 +1,9 @@
 from itertools import combinations
 I = raw_input().split()
 m = list()
-for i in xrange(1,int(I[1])):
+I[0] = list(I[0])
+I[0].sort()
+for i in xrange(1,int(I[1])+1):
     m.append(list(map(''.join,combinations(I[0],i))))
 
 for i in m:
