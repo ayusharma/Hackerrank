32422ccaa7b82b7e7d4c1b4974755c6fadf8a600
ayush sharma <ayush.aceit@gmail.com>
2015-12-21 19:31:55 +0530

    Ordered dict and counter

---
+++ collections_word_problem.py
@@ -0,0 +1,55 @@
+"""
+Problem Statement
+
+You are given n words. Some words may repeat. For each word, output its number of occurrences. The output order should correspond with the input order of appearance of the word. See the sample input/output for clarification.
+
+Note: Each input line ends with a  \ n  character.
+
+Constraints: 
+1≤n≤105 
+The sum of the lengths of all the words do not exceed 106 
+All the words are composed of lowercase English letters only.
+
+Input Format
+
+The first line contains the integer, n. 
+The next n lines each contain a word.
+
+Output Format
+
+Output 2 lines. 
+On the first line, output the number of distinct words from the input. 
+On the second line, output the number of occurrences for each distinct word according to their appearance in the input.
+
+Sample Input
+
+4
+bcdef
+abcdefg
+bcde
+bcdef
+Sample Output
+
+3
+2 1 1
+Explanation
+
+There are 3 distinct words. Here, "bcdef" appears twice in the input at the first and last positions. The other words appear once each. The order of the first appearances are "bcdef", "abcdefg" and "bcde" which corresponds to the output.
+
+"""
+
+from collections import Counter,OrderedDict
+n = input()
+l = list()
+lx = list()
+od = OrderedDict()
+for i in xrange(n):
+    kx = raw_input().split()[0]
+    od[kx] = i
+    l.append(kx)
+c = Counter(l)
+#print od
+for i in od:
+    lx.append(str(c[i]))
+print len(c)
+print ' '.join(lx)
