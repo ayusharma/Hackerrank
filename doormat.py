ce9fc04a5cb36b3ff69caddc36c1d4e97ffee47e
rakesh6451 <rakeshganeshula@gmail.com>
2020-05-11 15:51:44 +0530

    code written in python3 without using xrange straight forward with simple syntax

--- doormat.py
+++ doormat.py
@@ -1,14 +1,14 @@
 """
 
-Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat. 
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat.
 Design specifications are as follows:
 1. Size of mat must be NXM. (N is an odd natural number and M is 3 times N.)
-2. Design should have 'WELCOME' written in the center. 
+2. Design should have 'WELCOME' written in the center.
 3. Design pattern should only use '|', '.' and '-' characters.
 
 Sample Designs
 
-    Size: 7 x 21 
+    Size: 7 x 21
     ---------.|.---------
     ------.|..|..|.------
     ---.|..|..|..|..|.---
@@ -16,7 +16,7 @@ Sample Designs
     ---.|..|..|..|..|.---
     ------.|..|..|.------
     ---------.|.---------
-    
+
     Size: 11 x 33
     ---------------.|.---------------
     ------------.|..|..|.------------
@@ -45,9 +45,11 @@ Sample Designs
 """
 
 
-N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
-for i in xrange(0,N/2): 
-    print ('.|.'*i).rjust((M-2)/2,'-')+'.|.'+('.|.'*i).ljust((M-2)/2,'-')
-print 'WELCOME'.center(M,'-')
-for i in reversed(xrange(0,N/2)): 
-    print ('.|.'*i).rjust((M-2)/2,'-')+'.|.'+('.|.'*i).ljust((M-2)/2,'-')
+n,m = map(int,input().split())
+for i in range(n//2):
+    print(((2*i+1)*".|.").center(m,"-"))
+print("WELCOME".center(m,"-"))
+p=n-2
+while(p>0):
+    print((p*".|.").center(m,"-"))
+    p=p-2
