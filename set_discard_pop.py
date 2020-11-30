969f590a9bef67fac074dd8a20eeaf0429bb9e34
jyoti-2 <63103873+jyoti-2@users.noreply.github.com>
2020-11-30 08:03:28 +0530

    Code has been written in Python 3
    It's executing all the test cases

--- set_discard_pop.py
+++ set_discard_pop.py
@@ -96,20 +96,20 @@ Note : Convert elements of set s to integers while assigning. To ensure proper i
 
 """
 
-n = input()
-s = set(map(int, raw_input().split())) 
-a = input()
+n = int(input())
+s = set(map(int, input().split()))
+a = int(input())
 
-for i in xrange(a):
-	k = []
-	k = raw_input().split()
-	if k[0] == 'pop':
-		s.pop()
-	elif k[0] == 'remove':
-		s.remove(int(k[1]))
-	elif k[0] == 'discard':
-		s.discard(int(k[1]))
-	else:
-		print 'not a command'
+for i in range(a):
+    k = []
+    k = input().split()
+    if k[0] == 'pop':
+        s.pop()
+    elif k[0] == 'remove':
+        s.remove(int(k[1]))
+    elif k[0] == 'discard':
+        s.discard(int(k[1]))
+    else:
+        print ('not a command')
 
-print sum(s)
+print(sum(s))
