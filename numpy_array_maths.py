85481600c0296f33ffad161df3f82a23ae7a7d12
Mahesh Jadhav <58790433+ursmaheshj@users.noreply.github.com>
2021-04-14 15:33:14 +0530

    Updated numpy_array_maths.py
    Updated code according to python3 standards and fixed some bugs

--- numpy_array_maths.py
+++ numpy_array_maths.py
@@ -65,16 +65,16 @@ Use // for division in Python 3.
 
 
 import numpy
-k = map(int,raw_input().split())
+k = list(map(int,input().split()))
 a,b = [],[]
-for i in xrange(k[0]):
-    a.append(map(int,raw_input().split()))
-for i in xrange(k[0]):
-    b.append(map(int,raw_input().split()))
+for i in range(k[0]):
+    a.append(list(map(int,input().split())))
+for i in range(k[0]):
+    b.append(list(map(int,input().split())))
 A,B = numpy.array(a),numpy.array(b)
-print numpy.add(A,B)
-print numpy.subtract(A,B)
-print numpy.multiply(A,B)
-print numpy.divide(A,B)
-print numpy.mod(A,B)
-print numpy.power(A,B)
+print (numpy.add(A,B))
+print (numpy.subtract(A,B))
+print (numpy.multiply(A,B))
+print (numpy.floor_divide(A,B))
+print (numpy.mod(A,B))
+print (numpy.power(A,B))
